b8db9d71df3aa73ad0896fefc01504ad654c541d
Chad Humphries <chadrox999@gmail.com>
2019-09-09 19:15:54 -0700

    Calculator v1.0

---
+++ src/com/grunclepug/consoleapps/calculator/Calculator.java
@@ -0,0 +1,54 @@
+package com.grunclepug.consoleapps.calculator;
+
+import java.util.Scanner;
+
+public class Calculator
+{
+    //Variables
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        int number1;
+        int number2;
+        String operation;
+
+        String decision = "y";
+        while(decision.equalsIgnoreCase("y"))
+        {
+            System.out.println("First number: ");
+            number1 = sc.nextInt();
+
+            System.out.println("Second number: ");
+            number2 = sc.nextInt();
+
+            System.out.println("Operation: ");
+            operation = sc.next();
+
+            System.out.println("Answer: " + calculate(number1, number2, operation));
+
+            //Check if user want's to continue
+            System.out.println("Would you like to continue? (Y/N)");
+            decision = sc.next();
+        }
+    }
+
+    private static int calculate(int num1, int num2, String op)
+    {
+        int answer = 0;
+
+        switch(op)
+        {
+            case "+":
+                return answer = (num1 + num2);
+            case "-":
+                return answer = (num1 - num2);
+            case "*":
+                return answer = (num1 * num2);
+            case "/":
+                return answer = (num1 / num2);
+
+            default: return 0;
+        }
+    }
+}
